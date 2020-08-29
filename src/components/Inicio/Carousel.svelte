7db60ed9c4f9c4cae4ef662d469aac67adc8f2f1
Juan Tavarez <tavarezju@fondesa.org>
2020-08-29 17:52:05 -0400

    Diseño inicio

---
+++ src/components/Inicio/Carousel.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Carousel from "@beyonk/svelte-carousel";
+</script>
+
+
+<main>
+  <Carousel>
+    <div class="slide-content">
+      <img src="residencia.jpg" alt="Kitten 1" />
+    </div>
+    <div class="slide-content">
+      <img src="residencia2.jpg" alt="Kitten 1" />
+    </div>
+    <div class="slide-content">
+      <img src="ResidenciaH.jpg" alt="Kitten 1" />
+    </div>
+  </Carousel>
+
+  </main>
---
+++ src/components/Inicio/Servicios.svelte
@@ -0,0 +1,99 @@
+<script>
+  import Icon from "svelte-awesome/components/Icon.svelte";
+  import { wrench } from "svelte-awesome/icons";
+</script>
+
+<main>
+  <section class="hero is-primary is-light is-bold ">
+    <div class="hero-body">
+      <h1 class="title has-text-centered">
+        <Icon data={wrench} scale="2" />
+        Servicios
+      </h1>
+      <div class="container">
+        <!-- Prueba -->
+
+        <div class="tile is-ancestor">
+
+          <div class="tile mr-3 mt-3">
+
+            <article class="message is-primary">
+              <div class="message-body">
+              
+              <figure class="image is-128x128 container mb-3">
+                <img
+                  class="is-rounded"
+                  src="https://bulma.io/images/placeholders/128x128.png"
+                  alt="" />
+              </figure>
+              
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <strong>Pellentesque risus mi</strong>
+                , tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus
+                ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+                efficitur. Aenean ac
+                <em>eleifend lacus</em>
+                , in mollis lectus. Donec sodales, arcu et sollicitudin
+                porttitor, tortor urna tempor ligula, id porttitor mi magna a
+                neque. Donec dui urna, vehicula et sem eget, facilisis sodales
+                sem.
+              </div>
+            </article>
+          </div>
+
+          <div class="tile mr-3 mt-3">
+            <article class="message is-primary">
+              <div class="message-body">
+
+              <figure class="image is-128x128 container mb-3">
+                <img
+                  class="is-rounded"
+                  src="https://bulma.io/images/placeholders/128x128.png"
+                  alt="" />
+              </figure>
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <strong>Pellentesque risus mi</strong>
+                , tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus
+                ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+                efficitur. Aenean ac
+                <em>eleifend lacus</em>
+                , in mollis lectus. Donec sodales, arcu et sollicitudin
+                porttitor, tortor urna tempor ligula, id porttitor mi magna a
+                neque. Donec dui urna, vehicula et sem eget, facilisis sodales
+                sem.
+              </div>
+            </article>
+          </div>
+
+          <div class="tile mr-3 mt-3">
+            <article class="message is-primary">
+              <div class="message-body">
+
+              <figure class="image is-128x128 container mb-3">
+                <img
+                  class="is-rounded"
+                  src="https://bulma.io/images/placeholders/128x128.png"
+                  alt="" />
+              </figure>
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <strong>Pellentesque risus mi</strong>
+                , tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus
+                ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+                efficitur. Aenean ac
+                <em>eleifend lacus</em>
+                , in mollis lectus. Donec sodales, arcu et sollicitudin
+                porttitor, tortor urna tempor ligula, id porttitor mi magna a
+                neque. Donec dui urna, vehicula et sem eget, facilisis sodales
+                sem.
+              </div>
+            </article>
+          </div>
+
+        </div>
+        <!-- Prueba -->
+      </div>
+    </div>
+  </section>
+</main>
